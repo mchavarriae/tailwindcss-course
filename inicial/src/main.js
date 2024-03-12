3a615a94650f9777ece0b9344793e42aa15b1689
Marlon Chavarria <marlonc@tevixmd.com>
2024-03-11 21:16:05 -0600

    creacion de version base para trabajar

--- inicial/src/main.js
+++
@@ -1,9 +0,0 @@
-import { loadAll } from "./results.js";
-import initMenu from "./menu";
-import initDialog from "./dialog";
-
-document.addEventListener("DOMContentLoaded", async (e) => {
-  initMenu();
-  initDialog();
-  await loadAll();
-});
